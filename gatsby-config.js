eaff24515829f4baaaec793b6a8552fb200d534e
Allan Reyes <chef@ulampinoy.com>
2019-08-06 13:49:19 +0200

    Correct icon filename

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/madbul-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon-madbul.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
